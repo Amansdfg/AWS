067c1307eaac45502d9fac80b286610dcf7646ec
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-09-25 14:02:58 +0500

    fix  bugs

--- src/main/java/kz/kalabay/aws/S3/S3Service.java
+++ src/main/java/kz/kalabay/aws/S3/S3Service.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.model.S3Object;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class S3Service {
     private final S3Client s3Client;
@@ -36,5 +42,16 @@ public class S3Service {
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
         }
+    }
+    public List<String> listFiles() {
+        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .build();
+
+        ListObjectsV2Response listObjectsResponse = s3Client.listObjectsV2(listObjectsRequest);
+
+        return listObjectsResponse.contents().stream()
+                .map(S3Object::key)
+                .collect(Collectors.toList());
     }
 }
